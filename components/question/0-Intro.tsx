40bfe55e4c62f17fe38249dd7d42a7752dc2d400
Jupalaja <jupalaja@gmail.com>
2024-05-20 09:38:36 -0500

    added video

--- components/question/0-Intro.tsx
+++ components/question/0-Intro.tsx
@@ -42,18 +42,18 @@ export function Intro() {
         <br />
         <br />
       </QuestionBoxPara>
-      {/* <div className="video-container">
+      <div className="video-container">
         <iframe
           id="youtube-video-iframe"
           key={videoKey}
           width="100%"
           height="350"
-          src="https://www.youtube.com/embed/4oTTu-SMl1s"
+          src="https://www.youtube.com/embed/1H6euFEaTVs"
           title="YouTube video player"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture "
           allowFullScreen
         ></iframe>
-      </div> */}
+      </div>
 
       <BtnContainer
         showPressEnter={true}
